6d5643c7e77950c3df447a180a7718a0097ac9b3
Henrique Pagno de Lima <48952086+henriquepagno@users.noreply.github.com>
2026-03-01 00:36:36 +0000

    Configuração.

---
+++ .net-playground/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var solution = new Solution();
+
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+
+        var result = solution.TwoSum(nums, target);
+
+        Console.WriteLine($"[{string.Join(", ", result)}]");
+    }
+}
